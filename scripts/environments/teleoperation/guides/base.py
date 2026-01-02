f46208c1ebf6c917ad73906e1e7ed42e1b45e651
sashansamarajeewa <sashan1995@gmail.com>
2026-01-02 17:58:55 +0100

    Modify name tag logic

--- scripts/environments/teleoperation/guides/base.py
+++ scripts/environments/teleoperation/guides/base.py
@@ -557,7 +557,7 @@ class NameTagManager:
         widget_cls=NameTagWidget,
         width: float = 0.2,
         height: float = 0.1,
-        resolution_scale: int = 10,
+        resolution_scale: int = 20,
         unit_to_pixel_scale: int = 30,
         z_offset: float = 0.1,
         rotation_deg_xyz: Gf.Vec3d = Gf.Vec3d(90, 0, 0),
