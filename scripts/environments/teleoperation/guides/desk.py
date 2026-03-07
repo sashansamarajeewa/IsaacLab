600b9f2f586276d5e58a1d861141d6b0eef31556
sashansamarajeewa <sashan1995@gmail.com>
2026-03-07 12:42:53 +0100

    Modify next button logic for desk

--- scripts/environments/teleoperation/guides/desk.py
+++ scripts/environments/teleoperation/guides/desk.py
@@ -46,9 +46,9 @@ class DeskGuide(BaseGuide):
         1: ["DeskTop"],
         2: ["DeskTop", "FrontRightLeg"],
         3: ["DeskTop", "FrontRightLeg", "FrontLeftLeg"],
-        4: ["DeskTop", "FrontRightLeg", "FrontLeftLeg", "DeskTop"],
-        5: ["DeskTop", "FrontRightLeg", "FrontLeftLeg", "DeskTop", "BackRightLeg"],
-        6: ["DeskTop", "FrontRightLeg", "FrontLeftLeg", "DeskTop", "BackRightLeg", "BackLeftLeg"],
+        4: ["DeskTop"],
+        5: ["DeskTop", "BackRightLeg"],
+        6: ["DeskTop", "BackRightLeg", "BackLeftLeg"],
     }
 
     tol_z_dbox_t = 1.082  # distance between desk top and table origin along Z
